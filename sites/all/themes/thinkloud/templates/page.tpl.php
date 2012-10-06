44316ee4d317d0c6ec7d75aa83fdec7f15c5f586
Scared.AW <scared.aw@gmail.com>
2012-10-06 22:36:45 +0000

    theming

--- sites/all/themes/thinkloud/templates/page.tpl.php
+++ sites/all/themes/thinkloud/templates/page.tpl.php
@@ -182,31 +182,37 @@
       </div></div> <!-- /.section, /#sidebar-first -->
     <?php endif; ?>
 
-    <div id="content" class="column"><div class="section">
-      <?php if ($page['highlighted']): ?><div id="highlighted"><?php print render($page['highlighted']); ?></div><?php endif; ?>
-      <a id="main-content"></a>
-      <?php print render($title_prefix); ?>
-      <?php if ($title): ?>
-        <h1 class="title" id="page-title">
-          <?php print $title; ?>
-        </h1>
-      <?php endif; ?>
-      <?php print render($title_suffix); ?>
-      <?php if ($tabs): ?>
-        <div class="tabs">
-          <?php print render($tabs); ?>
+    <div id="content" class="column">
+	    <div class="section">
+	      <?php if ($page['highlighted']): ?>
+		    <div id="highlighted"><?php print render($page['highlighted']); ?></div>
+		  <?php endif; ?>
+	      <a id="main-content"></a>
+	      <?php print render($title_prefix); ?>
+	      <?php if ($title): ?>
+	        <h1 class="title" id="page-title">
+	          <?php print $title; ?>
+	        </h1>
+	      <?php endif; ?>
+	      <?php print render($title_suffix); ?>
+	      <?php if ($tabs): ?>
+	        <div class="tabs">
+	          <?php print render($tabs); ?>
+	        </div>
+	      <?php endif; ?>
+	      <?php print render($page['help']); ?>
+	      <?php if ($action_links): ?>
+	        <ul class="action-links">
+	          <?php print render($action_links); ?>
+	        </ul>
+	      <?php endif; ?>
+<!--		    <div id="first-column-content">-->
+	            <?php print render($page['content']); ?>
+<!--			</div>-->
+	      <?php print $feed_icons; ?>
         </div>
-      <?php endif; ?>
-      <?php print render($page['help']); ?>
-      <?php if ($action_links): ?>
-        <ul class="action-links">
-          <?php print render($action_links); ?>
-        </ul>
-      <?php endif; ?>
-      <?php print render($page['content']); ?>
-      <?php print $feed_icons; ?>
-
-    </div></div> <!-- /.section, /#content -->
+    </div>
+	  <!-- /.section, /#content -->
 
     <?php if ($page['sidebar_second']): ?>
       <div id="sidebar-second" class="column sidebar"><div class="section">
