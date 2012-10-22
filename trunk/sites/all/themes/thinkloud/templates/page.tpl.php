d878ced4f0e84b236526d7300206da8174172275
scared.aw@gmail.com <scared.aw@gmail.com@89b42fe5-cab4-ac25-e7f1-287d1a229314>
2012-10-22 00:59:49 +0000

    fixed theme

--- trunk/sites/all/themes/thinkloud/templates/page.tpl.php
+++ trunk/sites/all/themes/thinkloud/templates/page.tpl.php
@@ -12,55 +12,55 @@
  *
  * General utility variables:
  * - $base_path: The base URL path of the Drupal installation. At the very
- *   least, this will always default to /.
+ *	 least, this will always default to /.
  * - $directory: The directory the template is located in, e.g. modules/system
- *   or themes/bartik.
+ *	 or themes/bartik.
  * - $is_front: TRUE if the current page is the front page.
  * - $logged_in: TRUE if the user is registered and signed in.
  * - $is_admin: TRUE if the user has permission to access administration pages.
  *
  * Site identity:
  * - $front_page: The URL of the front page. Use this instead of $base_path,
- *   when linking to the front page. This includes the language domain or
- *   prefix.
+ *	 when linking to the front page. This includes the language domain or
+ *	 prefix.
  * - $logo: The path to the logo image, as defined in theme configuration.
  * - $site_name: The name of the site, empty when display has been disabled
- *   in theme settings.
+ *	 in theme settings.
  * - $site_slogan: The slogan of the site, empty when display has been disabled
- *   in theme settings.
+ *	 in theme settings.
  * - $hide_site_name: TRUE if the site name has been toggled off on the theme
- *   settings page. If hidden, the "element-invisible" class is added to make
- *   the site name visually hidden, but still accessible.
+ *	 settings page. If hidden, the "element-invisible" class is added to make
+ *	 the site name visually hidden, but still accessible.
  * - $hide_site_slogan: TRUE if the site slogan has been toggled off on the
- *   theme settings page. If hidden, the "element-invisible" class is added to
- *   make the site slogan visually hidden, but still accessible.
+ *	 theme settings page. If hidden, the "element-invisible" class is added to
+ *	 make the site slogan visually hidden, but still accessible.
  *
  * Navigation:
  * - $main_menu (array): An array containing the Main menu links for the
- *   site, if they have been configured.
+ *	 site, if they have been configured.
  * - $secondary_menu (array): An array containing the Secondary menu links for
- *   the site, if they have been configured.
+ *	 the site, if they have been configured.
  * - $breadcrumb: The breadcrumb trail for the current page.
  *
  * Page content (in order of occurrence in the default page.tpl.php):
  * - $title_prefix (array): An array containing additional output populated by
- *   modules, intended to be displayed in front of the main title tag that
- *   appears in the template.
+ *	 modules, intended to be displayed in front of the main title tag that
+ *	 appears in the template.
  * - $title: The page title, for use in the actual HTML content.
  * - $title_suffix (array): An array containing additional output populated by
- *   modules, intended to be displayed after the main title tag that appears in
- *   the template.
+ *	 modules, intended to be displayed after the main title tag that appears in
+ *	 the template.
  * - $messages: HTML for status and error messages. Should be displayed
- *   prominently.
+ *	 prominently.
  * - $tabs (array): Tabs linking to any sub-pages beneath the current page
- *   (e.g., the view and edit tabs when displaying a node).
+ *	 (e.g., the view and edit tabs when displaying a node).
  * - $action_links (array): Actions local to the page, such as 'Add menu' on the
- *   menu administration interface.
+ *	 menu administration interface.
  * - $feed_icons: A string of all feed icons for the current page.
  * - $node: The node object, if there is an automatically-loaded node
- *   associated with the page, and the node ID is the second argument
- *   in the page's path (e.g. node/12345 and node/12345/revisions, but not
- *   comment/reply/12345).
+ *	 associated with the page, and the node ID is the second argument
+ *	 in the page's path (e.g. node/12345 and node/12345/revisions, but not
+ *	 comment/reply/12345).
  *
  * Regions:
  * - $page['header']: Items for the header region.
@@ -87,73 +87,73 @@
 ?>
 <div id="page-wrapper"><div id="page">
 
-  <div id="header" class="<?php print $secondary_menu ? 'with-secondary-menu': 'without-secondary-menu'; ?>"><div class="section clearfix">
-      <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" id="logo">
-        <img src="/sites/all/themes/thinkloud/logo.png" alt="<?php print t('Home'); ?>" />
-      </a>
+	<div id="header" class="<?php print $secondary_menu ? 'with-secondary-menu': 'without-secondary-menu'; ?>"><div class="section clearfix">
+			<a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" id="logo">
+				<img src="/sites/all/themes/thinkloud/logo.png" alt="<?php print t('Home'); ?>" />
+			</a>
 
-    <?php if ($site_name || $site_slogan): ?>
-      <div id="name-and-slogan"<?php if ($hide_site_name && $hide_site_slogan) { print ' class="element-invisible"'; } ?>>
+		<?php if ($site_name || $site_slogan): ?>
+			<div id="name-and-slogan"<?php if ($hide_site_name && $hide_site_slogan) { print ' class="element-invisible"'; } ?>>
 
-        <?php if ($site_name): ?>
-          <?php if ($title): ?>
-            <div id="site-name"<?php if ($hide_site_name) { print ' class="element-invisible"'; } ?>>
-              <strong>
-                <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home"><span><?php print $site_name; ?></span></a>
-              </strong>
-            </div>
-          <?php else: /* Use h1 when the content title is empty */ ?>
-            <h1 id="site-name"<?php if ($hide_site_name) { print ' class="element-invisible"'; } ?>>
-              <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home"><span><?php print $site_name; ?></span></a>
-            </h1>
-          <?php endif; ?>
-        <?php endif; ?>
+				<?php if ($site_name): ?>
+					<?php if ($title): ?>
+						<div id="site-name"<?php if ($hide_site_name) { print ' class="element-invisible"'; } ?>>
+							<strong>
+								<a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home"><span><?php print $site_name; ?></span></a>
+							</strong>
+						</div>
+					<?php else: /* Use h1 when the content title is empty */ ?>
+						<h1 id="site-name"<?php if ($hide_site_name) { print ' class="element-invisible"'; } ?>>
+							<a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home"><span><?php print $site_name; ?></span></a>
+						</h1>
+					<?php endif; ?>
+				<?php endif; ?>
 
-        <?php if ($site_slogan): ?>
-          <div id="site-slogan"<?php if ($hide_site_slogan) { print ' class="element-invisible"'; } ?>>
-            <?php print $site_slogan; ?>
-          </div>
-        <?php endif; ?>
+				<?php if ($site_slogan): ?>
+					<div id="site-slogan"<?php if ($hide_site_slogan) { print ' class="element-invisible"'; } ?>>
+						<?php print $site_slogan; ?>
+					</div>
+				<?php endif; ?>
 
-      </div> <!-- /#name-and-slogan -->
-    <?php endif; ?>
+			</div> <!-- /#name-and-slogan -->
+		<?php endif; ?>
 
-    <?php print render($page['header']); ?>
+		<?php print render($page['header']); ?>
 
-<!--    --><?php //if ($main_menu): ?>
-<!--      <div id="main-menu" class="navigation">-->
-<!--        --><?php //print theme('links__system_main_menu', array(
-//          'links' => $main_menu,
-//          'attributes' => array(
-//            'id' => 'main-menu-links',
-//            'class' => array('links', 'clearfix'),
-//          ),
-//          'heading' => array(
-//            'text' => t('Main menu'),
-//            'level' => 'h2',
-//            'class' => array('element-invisible'),
-//          ),
-//        )); ?>
-<!--      </div> <!-- /#main-menu -->
-<!--    --><?php //endif; ?>
+<!--		--><?php //if ($main_menu): ?>
+<!--			<div id="main-menu" class="navigation">-->
+<!--				--><?php //print theme('links__system_main_menu', array(
+//					'links' => $main_menu,
+//					'attributes' => array(
+//						'id' => 'main-menu-links',
+//						'class' => array('links', 'clearfix'),
+//					),
+//					'heading' => array(
+//						'text' => t('Main menu'),
+//						'level' => 'h2',
+//						'class' => array('element-invisible'),
+//					),
+//				)); ?>
+<!--			</div> <!-- /#main-menu -->
+<!--		--><?php //endif; ?>
 
-    <?php if ($secondary_menu): ?>
-      <div id="secondary-menu" class="navigation">
-        <?php print theme('links__system_secondary_menu', array(
-          'links' => $secondary_menu,
-          'attributes' => array(
-            'id' => 'secondary-menu-links',
-            'class' => array('links', 'inline', 'clearfix'),
-          ),
-          'heading' => array(
-            'text' => t('Secondary menu'),
-            'level' => 'h2',
-            'class' => array('element-invisible'),
-          ),
-        )); ?>
-      </div>
-	  <!-- /#secondary-menu -->
-    <?php endif; ?>
+		<?php if ($secondary_menu): ?>
+			<div id="secondary-menu" class="navigation">
+				<?php print theme('links__system_secondary_menu', array(
+					'links' => $secondary_menu,
+					'attributes' => array(
+						'id' => 'secondary-menu-links',
+						'class' => array('links', 'inline', 'clearfix'),
+					),
+					'heading' => array(
+						'text' => t('Secondary menu'),
+						'level' => 'h2',
+						'class' => array('element-invisible'),
+					),
+				)); ?>
+			</div>
+		<!-- /#secondary-menu -->
+		<?php endif; ?>
 
 	<?php if ($tabs['#primary']):?>
 		<div id="primary-tabs">
@@ -162,8 +162,8 @@
 			</ul>
 		</div>
 	<?php endif;?>
-  </div>
-  </div>
+	</div>
+	</div>
 
 	<div id="fake-header"></div>
 	<?php if ($messages): ?>
@@ -174,98 +174,98 @@
 		</div>
 	<?php endif; ?>
 
-  <?php if ($page['featured']): ?>
-    <div id="featured"><div class="section clearfix">
-      <?php print render($page['featured']); ?>
-    </div></div> <!-- /.section, /#featured -->
-  <?php endif; ?>
+	<?php if ($page['featured']): ?>
+		<div id="featured"><div class="section clearfix">
+			<?php print render($page['featured']); ?>
+		</div></div> <!-- /.section, /#featured -->
+	<?php endif; ?>
 
-  <div id="main-wrapper" class="clearfix"><div id="main" class="clearfix">
+	<div id="main-wrapper" class="clearfix"><div id="main" class="clearfix">
 
-<!--    --><?php //if ($breadcrumb): ?>
-<!--      <div id="breadcrumb">--><?php //print $breadcrumb; ?><!--</div>-->
-<!--    --><?php //endif; ?>
+<!--		--><?php //if ($breadcrumb): ?>
+<!--			<div id="breadcrumb">--><?php //print $breadcrumb; ?><!--</div>-->
+<!--		--><?php //endif; ?>
 
-    <?php if ($page['sidebar_first']): ?>
-      <div id="sidebar-first" class="column sidebar">
-	      <div class="section">
-		      <?php if ($tabs['#secondary']):?>
-		      <div class="block secondary-tabs">
-			      <ul>
-				      <?php print render($tabs['#secondary']); ?>
-			      </ul>
-		      </div>
-			  <?php endif;?>
-            <?php print render($page['sidebar_first']); ?>
-	      </div>
-      </div>
-    <?php endif; ?>
+		<?php if ($page['sidebar_first']): ?>
+			<div id="sidebar-first" class="column sidebar">
+				<div class="section">
+					<?php if ($tabs['#secondary']):?>
+					<div class="block secondary-tabs">
+						<ul>
+							<?php print render($tabs['#secondary']); ?>
+						</ul>
+					</div>
+				<?php endif;?>
+						<?php print render($page['sidebar_first']); ?>
+				</div>
+			</div>
+		<?php endif; ?>
 
-    <div id="content" class="column">
-	    <div class="section">
-	      <?php if ($page['highlighted']): ?>
-		    <div id="highlighted"><?php print render($page['highlighted']); ?></div>
-		  <?php endif; ?>
-	      <a id="main-content"></a>
-	      <?php print render($title_prefix); ?>
-	      <?php if ($title): ?>
-	        <h1 class="title" id="page-title">
-	          <?php print $title; ?>
-	        </h1>
-	      <?php endif; ?>
-	      <?php print render($title_suffix); ?>
-<!--	      --><?php //if ($tabs): ?>
-<!--	        <div class="tabs">-->
-<!--	          --><?php //print render($tabs); ?>
-<!--	        </div>-->
-<!--	      --><?php //endif; ?>
-	      <?php print render($page['help']); ?>
-	      <?php if ($action_links): ?>
-	        <ul class="action-links">
-	          <?php print render($action_links); ?>
-	        </ul>
-	      <?php endif; ?>
-<!--		    <div id="first-column-content">-->
-	            <?php print render($page['content']); ?>
+		<div id="content" class="column">
+			<div class="section">
+				<?php if ($page['highlighted']): ?>
+				<div id="highlighted"><?php print render($page['highlighted']); ?></div>
+			<?php endif; ?>
+				<a id="main-content"></a>
+				<?php print render($title_prefix); ?>
+				<?php if ($title): ?>
+					<h1 class="title" id="page-title">
+						<?php print $title; ?>
+					</h1>
+				<?php endif; ?>
+				<?php print render($title_suffix); ?>
+<!--				--><?php //if ($tabs): ?>
+<!--					<div class="tabs">-->
+<!--						--><?php //print render($tabs); ?>
+<!--					</div>-->
+<!--				--><?php //endif; ?>
+				<?php print render($page['help']); ?>
+				<?php if ($action_links): ?>
+					<ul class="action-links">
+						<?php print render($action_links); ?>
+					</ul>
+				<?php endif; ?>
+<!--				<div id="first-column-content">-->
+							<?php print render($page['content']); ?>
 <!--			</div>-->
-	      <?php print $feed_icons; ?>
-        </div>
-    </div>
-	  <!-- /.section, /#content -->
+				<?php print $feed_icons; ?>
+				</div>
+		</div>
+		<!-- /.section, /#content -->
 
-    <?php if ($page['sidebar_second']): ?>
-      <div id="sidebar-second" class="column sidebar"><div class="section">
-        <?php print render($page['sidebar_second']); ?>
-      </div></div> <!-- /.section, /#sidebar-second -->
-    <?php endif; ?>
+		<?php if ($page['sidebar_second']): ?>
+			<div id="sidebar-second" class="column sidebar"><div class="section">
+				<?php print render($page['sidebar_second']); ?>
+			</div></div> <!-- /.section, /#sidebar-second -->
+		<?php endif; ?>
 
-  </div></div> <!-- /#main, /#main-wrapper -->
+	</div></div> <!-- /#main, /#main-wrapper -->
 
-  <?php if ($page['triptych_first'] || $page['triptych_middle'] || $page['triptych_last']): ?>
-    <div id="triptych-wrapper"><div id="triptych" class="clearfix">
-      <?php print render($page['triptych_first']); ?>
-      <?php print render($page['triptych_middle']); ?>
-      <?php print render($page['triptych_last']); ?>
-    </div></div> <!-- /#triptych, /#triptych-wrapper -->
-  <?php endif; ?>
+	<?php if ($page['triptych_first'] || $page['triptych_middle'] || $page['triptych_last']): ?>
+		<div id="triptych-wrapper"><div id="triptych" class="clearfix">
+			<?php print render($page['triptych_first']); ?>
+			<?php print render($page['triptych_middle']); ?>
+			<?php print render($page['triptych_last']); ?>
+		</div></div> <!-- /#triptych, /#triptych-wrapper -->
+	<?php endif; ?>
 
-  <div id="footer-wrapper"><div class="section">
+	<div id="footer-wrapper"><div class="section">
 
-    <?php if ($page['footer_firstcolumn'] || $page['footer_secondcolumn'] || $page['footer_thirdcolumn'] || $page['footer_fourthcolumn']): ?>
-      <div id="footer-columns" class="clearfix">
-        <?php print render($page['footer_firstcolumn']); ?>
-        <?php print render($page['footer_secondcolumn']); ?>
-        <?php print render($page['footer_thirdcolumn']); ?>
-        <?php print render($page['footer_fourthcolumn']); ?>
-      </div> <!-- /#footer-columns -->
-    <?php endif; ?>
+		<?php if ($page['footer_firstcolumn'] || $page['footer_secondcolumn'] || $page['footer_thirdcolumn'] || $page['footer_fourthcolumn']): ?>
+			<div id="footer-columns" class="clearfix">
+				<?php print render($page['footer_firstcolumn']); ?>
+				<?php print render($page['footer_secondcolumn']); ?>
+				<?php print render($page['footer_thirdcolumn']); ?>
+				<?php print render($page['footer_fourthcolumn']); ?>
+			</div> <!-- /#footer-columns -->
+		<?php endif; ?>
 
-    <?php if ($page['footer']): ?>
-      <div id="footer" class="clearfix">
-        <?php print render($page['footer']); ?>
-      </div> <!-- /#footer -->
-    <?php endif; ?>
+		<?php if ($page['footer']): ?>
+			<div id="footer" class="clearfix">
+				<?php print render($page['footer']); ?>
+			</div> <!-- /#footer -->
+		<?php endif; ?>
 
-  </div></div> <!-- /.section, /#footer-wrapper -->
+	</div></div> <!-- /.section, /#footer-wrapper -->
 
 </div></div> <!-- /#page, /#page-wrapper -->
