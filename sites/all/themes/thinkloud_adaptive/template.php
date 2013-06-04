b3b7e67ee6396baac3c790b4bb68d52993d90e4b
Scared.AW <scared.aw@gmail.com>
2013-06-04 14:36:01 +0000

    выпилил растягивалку коментов, запилил стиль для тэгов

--- sites/all/themes/thinkloud_adaptive/template.php
+++ sites/all/themes/thinkloud_adaptive/template.php
@@ -61,3 +61,28 @@ function thinkloud_adaptive_preprocess_page(&$variables){
 		$variables['attributes_array']['class'][] = 'profile-style-'.$acc->field_profile_style['und'][0]['tid'];
 	}
 }
+
+
+function thinkloud_adaptive_textarea($variables) {
+	$element = $variables['element'];
+	$element['#attributes']['name'] = $element['#name'];
+	$element['#attributes']['id'] = $element['#id'];
+	$element['#attributes']['cols'] = $element['#cols'];
+	$element['#attributes']['rows'] = $element['#rows'];
+	$element['#attributes']['style'] = array('opacity:0.6;');
+	_form_set_class($element, array('form-textarea'));
+
+	$wrapper_attributes = array(
+		'class' => array('form-textarea-wrapper'),
+	);
+
+	// Add resizable behavior.
+//	if (!empty($element['#resizable'])) {
+//		$wrapper_attributes['class'][] = 'resizable';
+//	}
+
+	$output = '<div' . drupal_attributes($wrapper_attributes) . '>';
+	$output .= '<textarea' . drupal_attributes($element['#attributes']) . '>' . check_plain($element['#value']) . '</textarea>';
+	$output .= '</div>';
+	return $output;
+}
